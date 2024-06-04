62e2e9e4bd083548511fd686cfb38f5b542ad5dd
Fridaxca123 <82528468+Fridaxca123@users.noreply.github.com>
2024-06-04 11:52:22 -0600

    subscribe.h

--- subscribe.h
+++ subscribe.h
@@ -4,32 +4,35 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+//Define la clase subscribe 
 class Subscribe {
-private:
-    std::string user_name;
+private: //Atributos privados 
+    string user_name;
     int user_age;
-    std::string user_email;
+    string user_email;
     int months;
 
-public:
-// Constructor por defecto
+public: //Inicia la seccion publica
+/*Constructor por defecto que inicializa los 
+atributos a cadenas vacías*/
 Subscribe() : user_name(""), user_age(0), user_email(""), months(0) {}
 
-// Constructor con parámetros
+/*Constructor que inicializa los atributos con los valores 
+proporcionados*/
 Subscribe(string usna, int usag, string usem, int mon):
  user_name(usna), user_age(usag), user_email(usem), months(mon) {}
 
-//Getters
-string get_user_name()const {return user_name;}
+// Métodos getter que retornan los valores de los atributos
+string get_user_name()const {return user_name;} //Retorna
 int get_user_age()const {return user_age;}
 string get_user_email()const {return user_email;}
 int get_months()const {return months;}
-int payment() const {
+float payment() const {
         return (months * 200) * 0.16;
     }
+// Método que muestra    
 void payment_info() {
-        cout<<"\nYou have succesfully subscirbed to spotify"<<endl;
+        cout<<"\nYou have succesfully subscirbed to spotify"<<endl;//imprime
         cout << "Name: " << user_name << endl;
         cout << "Total payment: " << payment() << endl;
         cout << "Please make your deposit to the following account: \nSpotify \nHSBC: SPO23456789"<<endl;
