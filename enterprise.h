58dc3912fcec00f05e3d476f1ff2fdecddbdb75e
Fridaxca123 <82528468+Fridaxca123@users.noreply.github.com>
2024-06-04 11:53:35 -0600

    enterprise.h

--- enterprise.h
+++ enterprise.h
@@ -4,20 +4,24 @@
 #include <string>
 using namespace std;
 
+//Declara clase enterprise
 class Enterprise {
+private: //Atributos privados de la clase
     string name;
     int year; 
     string CEO; 
     string location; 
-public:
+public: //Inicia la sccion publica
+//Constructor de la clase
 Enterprise(string n, int y, string c, string l){
-    name = n;
+    name = n; //Inicializan
     year = y;
     CEO = c;
     location = l;
 }
+//Metodo para mostrar informacion de la empresa 
 void show_info(){
-    cout << "Name: " << name << endl;
+    cout << "\nName: " << name << endl; //Imprime datos
     cout << "Year: " << year << endl;
     cout << "CEO: " << CEO << endl;
     cout << "Location: " << location << endl;
