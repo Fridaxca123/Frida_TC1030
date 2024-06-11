9042361b141bfec17288200064f4a57e85dbd89f
Frida Vargas <82528468+Fridaxca123@users.noreply.github.com>
2024-06-11 11:50:06 -0600

    Music.h

--- music.h
+++ music.h
@@ -8,7 +8,7 @@
 using namespace std;
 
 /*Define la clase de music que hereda de 
-la clase content*/
+la clase abstracta content*/
 class Music : public Content {
 private: //Atributos privados
     string album;
@@ -16,8 +16,11 @@ private: //Atributos privados
     int sec;
 
 public://Inicia la seccion publica
+//Constructor por defecto
+Music():Content("","", ""){}; //Sobrecarga 
+
 /*Constructor para Music, toma tanto los parametros
-de contructor y tambien los propios*/
+de content y tambien los propios*/
  Music(string name_cont, string author, string type_cont, string album, int min, int sec)
 : Content(name_cont, author, type_cont), album(album), min(min), sec(sec) {}
 
@@ -29,4 +32,5 @@ cout << "Duration: " << min << " minutes and " << sec << " seconds" << endl; }
 };
 
 #endif // MUSIC_H
+
 
